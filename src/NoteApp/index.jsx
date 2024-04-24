02f5c2abc3abaeeb8d87b0e34e5df7bb97fffb53
Ojo <oluwatobiojo2911@gmail.com>
2024-04-24 10:15:57 +0100

    root file of the note App

---
+++ src/NoteApp/index.jsx
@@ -0,0 +1,13 @@
+import CreateNewNote from "./CreateNewNote";
+import NoteList from "./NoteList";
+
+function index() {
+  return (
+    <>
+      <CreateNewNote />
+      <NoteList />
+    </>
+  );
+}
+
+export default index;
