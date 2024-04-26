553e91892554d5232ce76c60baf692b07f809e95
Ojo <oluwatobiojo2911@gmail.com>
2024-04-26 14:25:19 +0100

    I worked on responsiveness of the text font

--- src/ui/HeadingTag.jsx
+++ src/ui/HeadingTag.jsx
@@ -4,8 +4,8 @@ function Heading({ className, children, as = "h4" }) {
     h1: "text-[20px] md:text-[54px] tracking-widest",
     h2: "text-[18px] md:text-[48px] tracking-wider",
     h3: "text-[16px] md:text-[40px] tracking-wide",
-    h4: "text-[12px] md:text-[32px] tracking-normal",
-    h5: "text-[10px] md:text-[28px] tracking-normal",
+    h4: "text-[14px] md:text-[32px] tracking-normal",
+    h5: "text-[14px] md:text-[20px] tracking-normal",
   };
   const baseClassName = twMerge(" text-[#fafafa] ", sizes[as], className);
   return <h1 className={baseClassName}>{children}</h1>;
