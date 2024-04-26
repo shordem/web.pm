22c3711d61c1660b80d0aeba75755435d8538665
Ojo <oluwatobiojo2911@gmail.com>
2024-04-26 14:26:37 +0100

    I used the reusable showList component to refactor

--- src/settings/Organizations.jsx
+++ src/settings/Organizations.jsx
@@ -1,45 +1,104 @@
-import { useState } from "react";
-import { ArrowDown, ArrowUp } from "../ui/icons/arrow";
+import ShowList from "../ui/ShowList";
 import OrganizationName from "./OrganizationName";
-import SettingsText from "./SettingsText";
-import ButtonIcon from "../ui/ButtonIcon";
+
+// const organizationNameVariant = {
+//   open: {
+//     opacity: 1,
+//     y: 0,
+//     transition: { type: "spring", stiffness: 300, damping: 24 },
+//   },
+//   closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
+// };
 
 function Organizations() {
-  const [showOrganization, setShowOrganization] = useState(false);
-
-  function handleShowOrganization() {
-    setShowOrganization((sh) => !sh);
-  }
+  // variants={{
+  //   open: { rotate: 180 },
+  //   closed: { rotate: 0 }
+  // }}
+  // transition={{ duration: 0.2 }}
+  // style={{ originY: 0.55 }}
 
   return (
-    <li className="py-4 px-6 ">
-      {" "}
-      <div className=" flex items-center justify-between ">
-        <SettingsText>Organization</SettingsText>{" "}
-        <ButtonIcon onClick={handleShowOrganization}>
-          {" "}
-          {showOrganization ? (
-            <span>
-              {" "}
-              <ArrowDown />{" "}
-            </span>
-          ) : (
-            <span>
-              {" "}
-              <ArrowUp />{" "}
-            </span>
-          )}
-        </ButtonIcon>
-      </div>
-      {showOrganization && (
-        <ul className="flex flex-col gap-2 py-4">
-          <OrganizationName name={"Organization 1"} />
-          <OrganizationName name={"Organization 2"} />
-          <OrganizationName name={"Organization 3"} />
-        </ul>
-      )}
-    </li>
+    <ShowList ListTitle={"Organizations"}>
+      <OrganizationName name={"Organization 1"} />
+      <OrganizationName name={"Organization 2"} />
+      <OrganizationName name={"Organization 3"} />
+      <OrganizationName name={"Organization 3"} />
+    </ShowList>
   );
 }
 
 export default Organizations;
+
+// import "./styles.css";
+// import { useState } from "react";
+// import { motion, Variants } from "framer-motion";
+
+// const itemVariants: Variants = {
+//   open: {
+//     opacity: 1,
+//     y: 0,
+//     transition: { type: "spring", stiffness: 300, damping: 24 }
+//   },
+//   closed: { opacity: 0, y: 20, transition: { duration: 0.2 } }
+// };
+
+// export default function App() {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   return (
+//     <motion.nav
+//       initial={false}
+//       animate={isOpen ? "open" : "closed"}
+//       className="menu"
+//     >
+//       <motion.button
+//         whileTap={{ scale: 0.97 }}
+//         onClick={() => setIsOpen(!isOpen)}
+//       >
+//         Menu
+//         <motion.div
+//           variants={{
+//             open: { rotate: 180 },
+//             closed: { rotate: 0 }
+//           }}
+//           transition={{ duration: 0.2 }}
+//           style={{ originY: 0.55 }}
+//         >
+//           <svg width="15" height="15" viewBox="0 0 20 20">
+//             <path d="M0 7 L 20 7 L 10 16" />
+//           </svg>
+//         </motion.div>
+//       </motion.button>
+//       <motion.ul
+//         variants={{
+//           open: {
+//             clipPath: "inset(0% 0% 0% 0% round 10px)",
+//             transition: {
+//               type: "spring",
+//               bounce: 0,
+//               duration: 0.7,
+//               delayChildren: 0.3,
+//               staggerChildren: 0.05
+//             }
+//           },
+//           closed: {
+//             clipPath: "inset(10% 50% 90% 50% round 10px)",
+//             transition: {
+//               type: "spring",
+//               bounce: 0,
+//               duration: 0.3
+//             }
+//           }
+//         }}
+//         style={{ pointerEvents: isOpen ? "auto" : "none" }}
+//       >
+//         <motion.li variants={itemVariants}>Item 1 </motion.li>
+//         <motion.li variants={itemVariants}>Item 2 </motion.li>
+//         <motion.li variants={itemVariants}>Item 3 </motion.li>
+//         <motion.li variants={itemVariants}>Item 4 </motion.li>
+//         <motion.li variants={itemVariants}>Item 5 </motion.li>
+//       </motion.ul>
+//     </motion.nav>
+//   );
+// }
