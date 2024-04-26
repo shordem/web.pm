d5c67084601ccf87e6fba3961a7e3a646faae3c5
Ojo <oluwatobiojo2911@gmail.com>
2024-04-26 14:39:04 +0100

    added animations

--- src/TodoApp/index.jsx
+++ src/TodoApp/index.jsx
@@ -1,13 +1,24 @@
 import CreateNewTodo from "./CreateNewTodo";
 import ToDoList from "./ToDoList";
+import { motion } from "framer-motion";
+
+const todoAppVariant = {
+  hidden: { opacity: 0, x: "-100px" },
+  visible: { opacity: 1, x: 0 },
+};
 
 function TodoApp() {
   return (
-    <>
+    <motion.div
+      initial={"hidden"}
+      animate={"visible"}
+      exit={"hidden"}
+      variants={todoAppVariant}
+    >
       {" "}
       <CreateNewTodo />
       <ToDoList />
-    </>
+    </motion.div>
   );
 }
 
