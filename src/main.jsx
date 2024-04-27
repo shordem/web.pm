103efc943ce8a88a6c74dd0de771f6ffb2a423d0
Horlakz <officialhorlakz@gmail.com>
2024-04-27 14:20:45 +0100

    Refactor file names and imports for consistency

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { ErrorBoundary } from "react-error-boundary";
-import ErrorFallback from "./ui/ErrorFallback.jsx";
+import App from "./app.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ErrorBoundary
-    FallbackComponent={ErrorFallback}
-    onReset={() => window.location.replace("/")}
-  >
+  <React.StrictMode>
     <App />
-  </ErrorBoundary>
+  </React.StrictMode>
 );
