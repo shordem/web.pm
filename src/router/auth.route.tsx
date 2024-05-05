ba4c5cd3a124cdcd47ab8acd0b819d5b97f03c53
Horlakz <officialhorlakz@gmail.com>
2024-05-05 19:21:47 +0100

    refactor(/src/router/): updates router setup and separate

---
+++ src/router/auth.route.tsx
@@ -0,0 +1,6 @@
+import LoginPage from "@/pages/auth/login";
+
+export const authRoutes = [
+  { path: "/login", element: <LoginPage /> },
+  //   { path: "/register", element: <RegisterPage /> },
+];
