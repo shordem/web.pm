3e8612f93a0bcb056302fe8bcdb5e4b949805aae
Horlakz <officialhorlakz@gmail.com>
2024-05-06 03:56:57 +0100

    Add new colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
       colors: {
         vlg: "hsl(0, 0%, 98%)",
         primary: 'hsl(0, 0%, 10%)',
+        info: 'hsl(212, 86%, 64%)',
+        danger: 'hsl(0, 100%, 74%)',
+        success: 'hsl(154, 59%, 51%)',
+        warning: 'hsl(43, 100%, 51%)',
       },
     },
   },
