17f6d919cd14dc3012194f55b8169c2e9ef454ec
Horlakz <officialhorlakz@gmail.com>
2024-05-06 04:44:45 +0100

    Refactor app.tsx and query-client.tsx files

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,6 @@ import { Toaster } from "react-hot-toast";
 import "./styles/globals.scss";
 
 import QueryClientProvider from "./providers/query-client";
-import TodoProvider from "./providers/todo";
 import Router from "./router";
 import ErrorFallback from "./ui/ErrorFallback";
 
@@ -14,31 +13,31 @@ function App() {
       FallbackComponent={ErrorFallback}
       onReset={() => window.location.replace("/")}
     >
-      <TodoProvider>
-        <QueryClientProvider>
-          <Router />
+      {/* <TodoProvider> */}
+      <QueryClientProvider>
+        <Router />
 
-          <Toaster
-            position="bottom-left"
-            gutter={12}
-            containerStyle={{ margin: "8px" }}
-            toastOptions={{
-              success: {
-                duration: 3000,
-              },
-              error: { duration: 5000 },
-              style: {
-                fontSize: "16px",
-                maxWidth: "500px",
-                padding: "16px 24px",
-                // backgroundColor: "var(--color-grey-0)",
-                backgroundColor: "bg-red-500",
-                color: "bg-red-100",
-              },
-            }}
-          />
-        </QueryClientProvider>
-      </TodoProvider>
+        <Toaster
+          position="bottom-left"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: { duration: 5000 },
+            style: {
+              fontSize: "16px",
+              maxWidth: "500px",
+              padding: "16px 24px",
+              // backgroundColor: "var(--color-grey-0)",
+              backgroundColor: "bg-red-500",
+              color: "bg-red-100",
+            },
+          }}
+        />
+      </QueryClientProvider>
+      {/* </TodoProvider> */}
     </ErrorBoundary>
   );
 }
