cff2eb4333ef00392aa9b1bacfb280d3482a679b
Horlakz <officialhorlakz@gmail.com>
2024-05-07 10:40:00 +0100

    feat(/src/pages/): updates auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <main className="w-screen h-screen flex items-center justify-center">
-      <section className="bg-gray-200 p-20 text-center rounded">
+      <section className="text-center">
         <h1 className="text-3xl">Home Page</h1>
-        <Link to="/dashboard" className="text-purple-700">
+        <Link to="/dashboard" className="text-secondary">
           Go to Dashboard
         </Link>
       </section>
