04f44f2caaebc25ac125d8262c4781289cd2e316
Horlakz <officialhorlakz@gmail.com>
2024-05-07 10:42:16 +0100

    Fixes import issue and updates authentication logic in auth layout

--- src/pages/auth/layout.tsx
+++ src/pages/auth/layout.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
+import { useRouter } from "@/router/router.hook";
+import { useAuth } from "./auth.hook";
+
 function AuthLayout() {
+  const auth = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (auth.isAuthenticated) {
+      router.goTo("/dashboard");
+    }
+  }, [auth.isAuthenticated]);
   return (
     <main className="w-full h-screen flex items-center justify-center">
       <Outlet />
