ee9d3d076426660ab994476a7034769c698da641
Horlakz <officialhorlakz@gmail.com>
2024-05-07 10:02:00 +0100

    enhancement(tailwind config): adds bg-accent color to config theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         sans: ["Josefin Sans", "sans-serif"],
       },
       colors: {
-        vlg: "hsl(0, 0%, 98%)",
+        'bg-accent': '#262626',
         primary: '#4caf50',
         secondary: '#ffa500',
         info: '#3498db',
