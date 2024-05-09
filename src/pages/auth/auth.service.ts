df04dfb03665be0646c5dac8e71718588ca3dcb0
Ojo <oluwatobiojo2911@gmail.com>
2024-05-09 22:14:53 +0100

    megerd properly

--- src/pages/auth/auth.service.ts
+++ src/pages/auth/auth.service.ts
@@ -4,12 +4,8 @@ import {
   LoginResponsePayload,
   RegistrationRequestPayload,
   RegistrationResponsePayload,
-<<<<<<< HEAD:src/services/auth/index.ts
-  User,
-=======
   VerifyEmailRequestPayload,
   VerifyEmailResponsePayload,
->>>>>>> d097d808884255f0cbf8b3b8ee8f004b45ef5a39:src/pages/auth/auth.service.ts
 } from "./auth.interface";
 
 export class AuthService {
@@ -33,15 +29,10 @@ export class AuthService {
     >("auth/register", data);
   }
 
-<<<<<<< HEAD:src/services/auth/index.ts
-  user() {
-    return this.apiClient.get<User>("user");
-=======
   verifyEmail(data: VerifyEmailRequestPayload) {
     return this.apiClient.post<
       VerifyEmailRequestPayload,
       VerifyEmailResponsePayload
     >("auth/verify-email", data);
->>>>>>> d097d808884255f0cbf8b3b8ee8f004b45ef5a39:src/pages/auth/auth.service.ts
   }
 }
