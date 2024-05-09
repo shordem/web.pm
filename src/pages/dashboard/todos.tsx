3e03449b3308351e8caf6975f8da1cc0e9a2e59a
Ojo <oluwatobiojo2911@gmail.com>
2024-05-09 22:06:12 +0100

    started todo integration

--- src/pages/dashboard/todos.tsx
+++ src/pages/dashboard/todos.tsx
@@ -6,12 +6,42 @@ import { Button } from "@/components/ui/button";
 import Input from "@/components/ui/form/input";
 import TextArea from "@/components/ui/form/textarea";
 import Modal from "@/components/ui/modal";
+import { useDashboardContext } from "./dashboard-context";
+import { useCreateTodo, useGetTodos } from "./dashboard.hook";
 
 function Todos() {
+  // Context hook
+  const { currentOrganisationDetails } = useDashboardContext();
+
+  // Use state hook
   const [createTaskVisibility, setCreateTaskVisibility] = useState(false);
   const [updateTaskVisibility, setUpdateTaskVisibility] = useState(false);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+
+  // todo hooks
+  const todos = useGetTodos(
+    currentOrganisationDetails.id,
+    currentOrganisationDetails.folder?.id || ""
+  );
+  console.log(todos.data?.data);
+
+  const createTodo = useCreateTodo(
+    currentOrganisationDetails.id,
+    currentOrganisationDetails.folder?.id || ""
+  );
+
+  // create a date in 7 days time
+  function createDueDate() {
+    const date = new Date();
+    date.setDate(date.getDate() + 7);
+
+    let formattedDate = date.toISOString();
+    formattedDate = formattedDate.replace("T", " ");
+    formattedDate = formattedDate.replace("Z", "");
+
+    return formattedDate;
+  }
 
   return (
     <>
@@ -21,7 +51,21 @@ function Todos() {
       >
         <div className="grid justify-center gap-4 p-12">
           <h4 className="text-2xl">Add new todo</h4>
-          <form className="w-96 flex flex-col items-center gap-4">
+          <form
+            className="w-96 flex flex-col items-center gap-4"
+            onSubmit={(e) => {
+              e.preventDefault();
+              createTodo
+                .mutateAsync({
+                  title,
+                  description,
+                  due_date: createDueDate(),
+                })
+                .then(() => {
+                  setCreateTaskVisibility(false);
+                });
+            }}
+          >
             <Input
               label="Title"
               className="w-full"
@@ -34,7 +78,9 @@ function Todos() {
               value={description}
               onChange={(e) => setDescription(e.target.value)}
             />
-            <Button className="mt-4">Add todo</Button>
+            <Button className="mt-4" isLoading={createTodo.isPending}>
+              Add todo
+            </Button>
           </form>
         </div>
       </Modal>
