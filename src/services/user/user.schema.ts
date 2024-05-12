f95000bd84a17566b273dffaddc64e2b5c1cdf4d
Ojo <oluwatobiojo2911@gmail.com>
2024-05-12 16:22:35 +0100

    deleted unused files

--- src/services/user/user.schema.ts
+++
@@ -1,9 +0,0 @@
-import { object, string } from "yup";
-
-export const userSchema = object({
-  first_name: string().default(""),
-  last_name: string().default(""),
-  username: string().min(6).max(64).default(""),
-  email: string().email().required().default(""),
-  password: string().min(6).required().default(""),
-});
